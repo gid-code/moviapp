5bfebf740a483389b1c1b2735be5046dbb047c6a
gid-code <gideonpeprah94@gmail.com>
2020-07-31 00:11:03 +0000

    app files

---
+++ app/Http/Livewire/SearchDropdown.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Illuminate\Support\Facades\Http;
+use Livewire\Component;
+
+
+
+class SearchDropdown extends Component
+{
+    public $search = '';
+    public function render()
+    {
+        $searchResult = [];
+        if(strlen($this->search) > 2){
+
+            $searchResult = Http::withToken(config('services.tmdb.token'))
+                ->get(env('TMDB_BASEURL','https://api.themoviedb.org/3/').'/search/movie?query='.$this->search)
+                ->json()['results'];
+            // dump($searchResult);
+
+        }
+
+        return view('livewire.search-dropdown',[
+            'searchResult' => collect($searchResult)->take(7)
+        ]);
+
+    }
+}
